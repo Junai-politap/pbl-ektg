b17656bc1ea56d900a66f31083b5ca0bcd484d38
Junaidi <junaidiju4109@gmail.com>
2023-03-26 13:20:23 +0700

    First

---
+++ resources/views/components/layout/sidebar.blade.php
@@ -0,0 +1,16 @@
+<nav class="hk-nav hk-nav-light">
+    <a href="javascript:void(0);" id="hk_nav_close" class="hk-nav-close"><span class="feather-icon"><i data-feather="x"></i></span></a>
+    <div class="nicescroll-bar">
+        <div class="navbar-nav-wrap">
+            <ul class="navbar-nav flex-column">
+                <x-layout.sidebar.menu-item label="Dashboard" icon="ion ion-ios-keypad" />
+                <x-layout.sidebar.menu-item label="Pengelola" icon="ion ion-ios-person-add" />
+                <x-layout.sidebar.menu-item label="Desa Wisata" icon="ion ion-md-globe" />
+                <x-layout.sidebar.menu-item label="Atrakasi Wisata" icon="ion ion-ios-copy"/>
+                <x-layout.sidebar.menu-item label="Kalender Wisata" icon="ion ion-md-calendar"/>
+                <x-layout.sidebar.menu-item label="Kontak" icon="ion ion-md-call"/>
+               
+            </ul>
+        </div>
+    </div>
+</nav>
